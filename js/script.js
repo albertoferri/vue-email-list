0c03f3dc1a73c441dad82f45d9a24eb07c607c8f
albertoferri <falberto99@gmail.com>
2024-02-21 14:06:58 +0100

    aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -3,21 +3,25 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            mails: "",
+            mails: [],
         }
     },
     mounted() {
-        
+        // promise serve per far visualizzare l'array alla fine del ciclo
+        let promises = [];
         for(let i = 0; i < 10; i++){
-
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) =>{
-    
-                this.mails += result.data.response + " "
-    
-            })
+            promises.push(
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            );
         }
 
-
+        // push dentro l'array solo quando il ciclo prima è finito
+        Promise.all(promises).then((results) => {
+            results.forEach((result) => {
+                this.mails.push(result.data.response);
+            });
+            console.log(this.mails);
+        });
     },
 }).mount("#app");
 // ******** VUE ********
