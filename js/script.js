e9ca57cb8e835d868d2bfe3740250b20839a76cb
albertoferri <falberto99@gmail.com>
2024-02-21 13:44:33 +0100

    aggiunta prima stampa dell'API

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ createApp({
         return {
             mail: "",
         }
-    }
+    },
+    mounted() {
+        
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) =>{
+
+            this.mail = result.data.response
+
+        })
+
+
+    },
 }).mount("#app");
 // ******** VUE ********
